463b195bdb07b23ec67c27b5019feea0c6878be7
Piotr <piotr.koszarny@outlook.com>
2019-12-08 15:11:38 +0100

    move admin folder

--- app-client/src/app/app-routing.module.ts
+++ app-client/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{
   path: 'admin-panel',
-  loadChildren: () => import('../admin-panel/admin-panel.module').then(mod => mod.AdminPanelModule),
+  loadChildren: () => import('./admin-panel/admin-panel.module').then(mod => mod.AdminPanelModule),
 }];
 
 @NgModule({
